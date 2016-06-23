ab580d9863eebccb99a9e59c6af0d0a589c457cf
许炯勃 <xujiongbo@xujiongbodeMBP.lan>
2016-06-23 14:46:03 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,34 @@
+## 用法
+#### html
+```html
+<script type="text/javascript" src="path/aniframe.js"></script>
+```
+#### javscript
+```javascript
+let frame = new AniFrame({
+    target: document.getElementById('ani-frame'),
+    url: 'images/sprite.jpg',
+    totalFrame: 20
+})
+```
+
+## 选项(*为必需)
+* target: 必需，执行帧动画的元素
+* url: 必需，背景图片地址，图片必需为横排或竖排拼接的雪碧图
+* totalFrame: 必需，总的帧数
+* width: 单帧背景图宽度，默认为target元素宽度
+* height: 单帧背景图高度，默认为target元素高度
+* speed: 播放速度,即每帧停留时间，单位为ms毫秒，默认100
+* direction: 雪碧图的排列方向，默认为'x'(横排), 'y'则表示竖排
+* loop: 是否循环播放，默认为true
+#### 回调
+* onPlaying(current, total): 每帧播放回调，current为当前帧数(第一帧为0)，total为总帧数
+* onPlayEnd(): 播放结束的回调，若loop为true，则每个循环回调一次
+
+
+## 方法
+* play(): 开始播放
+* pause(): 暂停播放，下次播放从暂停位置开始
+* stop(): 停止播放，下次播放从第一帧开始
+* toggle(status): 切换播放状态，若当前为暂停或停止状态，则开始播放；若当前正在播放，则根据传入的status('pause'或'stop')执行相应动作
+
---
+++ aniframe.js
@@ -0,0 +1,78 @@
+(function(root){
+    function getStyle(el, name) {
+    　 　if(window.getComputedStyle) {
+    　 　    return window.getComputedStyle(el, null)
+    　 　}else{
+    　 　 　 return el.currentStyle
+    　 　}
+　   }
+    function AniFrame(options){
+        this.options = {
+            width: parseInt(getStyle(options.target).width),
+            height: parseInt(getStyle(options.target).height),
+            speed: 100,
+            direction: 'x',
+            loop: true,
+            onPlaying: function(){},
+            onPlayEnd: function(){}
+        }
+        $.extend(this.options, options)
+        this.init()
+        return this
+    }
+    AniFrame.prototype.init = function(){
+        var o = this.options
+        this.index = 0
+        this.timer = null
+        this.status = 'stopped'
+        if(o.direction === 'y'){
+            o.target.style.backgroundSize = o.width + 'px ' + (o.totalFrame * o.height) + 'px'
+        }else{
+            o.target.style.backgroundSize = (o.totalFrame * o.width) + 'px ' + o.height + 'px'
+        }
+        o.target.style.backgroundImage = 'url(' + o.url + ')'
+    }
+    AniFrame.prototype._play = function(){
+        var o = this.options
+        this.index++
+        if(this.index >= this.options.totalFrame){
+            o.onPlayEnd()
+            if(this.options.loop){
+                this.index = 0
+            }else{
+                this.stop()
+                return
+            }
+        }
+        if(o.direction === 'y'){
+            o.target.style.backgroundPosition = "0px -" + (o.height * this.index) + "px";
+        }else{
+            o.target.style.backgroundPosition = "-" + (o.width * this.index) + "px 0px";
+        }
+        o.onPlaying(this.index, o.totalFrame)
+    }
+    AniFrame.prototype.play = function(){
+        this.timer = setInterval(this._play.bind(this), this.options.speed)
+        this.status = 'playing'
+    }
+    AniFrame.prototype.pause = function(){
+        clearInterval(this.timer)
+        this.timer = null
+        this.status = 'paused'
+    }
+    AniFrame.prototype.stop = function(){
+        this.pause()
+        this.index = 0
+        this.status = 'stopped'
+    }
+    AniFrame.prototype.toggle = function(status){
+        if(this.status !== 'playing'){
+            this.play()
+        }else if(status === 'stop'){
+            this.stop()
+        }else{
+            this.pause()
+        }
+    }
+    root.AniFrame = AniFrame
+})(window)
